5b77e7d6be89f46a99924f4b7faf80a2b68108a7
ademuliadi <149946041+ademuliadi@users.noreply.github.com>
2024-05-22 09:47:53 +0800

    Add files via upload

---
+++ praktikum JS/main.js
@@ -0,0 +1,60 @@
+// alert ('halo')
+
+// console.log('halo')
+
+// prompt('masukan usia anda')
+
+ 
+// var usia = 999999
+// console.log('halo, usia kamu adalah '+ usia)
+
+// const usia = 45
+// let usia = 70
+// usia = 90
+// console.log('halo, usia kamu adalah '+ usia)
+
+// let usia = prompt('berapa usia kamu? ')
+// alert ('usia kamu adalah ' + usia)
+
+//let nama = 'Nathan'
+//let usia = 78
+//let tinggiBadan = 171.1
+//let hobi = ['nikah','photo']
+//let tempatLahir = {provinsi:'bali',kota:'Denpasar'}
+//let beratBadan = 81.2
+//let pacar = 2
+
+// if (pacar == null){pacar = 'belum punya'}
+// else {pacar = 'udah punya'}
+// switch (pacar){
+// case 1: pacar = "1 aja cukup";
+// break;
+// case 2: pacar = "2 ni bos senggol";
+// break; 
+// default: pacar = "belum punya ni cariin dong";
+// break; }
+//let saldoAwal = 50000
+//let saldoTambahan = 100000
+//const hutang = 900000
+//const saldoAkhir = saldoAwal + saldoTambahan - hutang
+//alert (` saya memiliki saldo awal itu Rp${saldoAwal}
+//dan dengan gaji sedikit sekitar Rp${saldoTambahan} dan saya punya hutang sekitar  Rp${hutang}
+//saldo saya sekarang adalah Rp${saldoAkhir}`)
+// console.log('halo, usia kamu adalah '+ usia, 'NAMA SAYA '+nama, 'Tinggi badan ' +tinggiBadan,'berat badan saya '+ beratBadan)
+// alert (`nama saya ${nama}, usia saya ${usia}
+// tinggi badan ${tinggiBadan}, hobi saya ${hobi}
+// tempat lahir ${tempatLahir.provinsi}, pacar saya ${pacar}`)
+
+
+// let namaMahasiswa = ['kucing','anjing','binatang']
+// namaMahasiswa.push('makan','minum')
+// namaMahasiswa.shift()
+// namaMahasiswa.pop()
+// alert (namaMahasiswa)
+
+
+// for (let i =1; i<= 5; i = i+1){console.log('ayo apa ini')}
+const namaMahasiswa=['kucing','anjing','binatang','minum','makan']
+for (let i = 0; i< namaMahasiswa.leght; i++){
+	console.log(`NIM :${i+1} nama : ${namaMahasiswa[i]}`); 
+}
